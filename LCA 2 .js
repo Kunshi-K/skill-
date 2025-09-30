7ac29cee393eeaa685e433ca06ec0715274edb31
Kunshi-K <kunshik9@gmail.com>
2025-09-30 10:25:43 +0530

    LCA 2 .js

---
+++ LCA 2 .js
@@ -0,0 +1,68 @@
+Even Numbers (Using while loop)
+
+i)Write a program to print all Odd numbers from 1 to 50 using a while loop
+let i = 1;
+while (i <= 50) {
+    console.log(i);
+    i += 2;  
+}
+Output : 
+1
+3
+5
+7
+9
+11
+13
+15
+17
+19
+21
+23
+25
+27
+29
+31
+33
+35
+37
+39
+41
+43
+45
+47
+49
+
+ii) Write a program to pront all even numbers from 50 down to 2 using while loop 
+let i = 50; 
+while (i >= 2) {   
+    console.log(i); 
+    i -= 2;   
+}
+Output : 
+50
+48
+46
+44
+42
+40
+38
+36
+34
+32
+30
+28
+26
+24
+22
+20
+18
+16
+14
+12
+10
+8
+6
+4
+2
+
